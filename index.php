6ed1dd37ba32baec3985e02b63fd9ef6b9c7c3ef
Priyanshu <priyanshu_s@outlook.com>
2019-04-12 02:18:17 +0530

    .

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="styles.css" />
+  </head>
+  <body>
+    <div class="main">
+      <div class = "logo">
+        <div class = "logo-container">
+        <div id = "title">Jeev Aashraya</div>
+        <div id = "address">Gomtinagar, Lucknow, Uttar Pradesh</div>
+        <div id = "contact">+91-8009392222</div>
+      </div>
+      </div>
+      <div class = "tab">
+      <button class="tablinks" onclick="smoothScroll(document.getElementById('content1'))">Content1</button>
+      <button class="tablinks" onclick="smoothScroll(document.getElementById('content2'))">Content2</button>
+      <button class="tablinks" onclick="smoothScroll(document.getElementById('content3'))">Content3</button>
+      <button class="tablinks" onclick="smoothScroll(document.getElementById('content4'))">Content4</button>
+      <button class="tablinks" onclick="smoothScroll(document.getElementById('content5'))">Content5</button>
+    </div>
+    <div class = "contents">
+  <div id ="content1">
+    <div class = "left">
+      <img src="imgs/content1.png" alt="Smiley face">
+    </div>
+    <div class = "right">
+      <p>
+        Jeev Aashraya (translation: a home for animals) was started in 2012 with
+        a single purpose -- to provide every voiceless with what they deserve:
+        love, affection and care.
+      </p>
+      <p>
+        We try to make sure that none of the strays in the city are in any sort
+        of pain and provide with all the intensive care they need.
+      </p>
+      <p>
+        Proin blandit ornare felis, sed molestie dui egestas ac. Etiam sagittis, augue vel euismod lacinia, elit dui commodo lectus, sed interdum lacus felis at neque. Ut sed vestibulum nunc. Nunc hendrerit venenatis pretium. Fusce tincidunt libero arcu, malesuada rutrum metus varius eu. Maecenas et vulputate ex, quis congue nunc. Fusce eu dui ornare, interdum nisl eu, aliquet ipsum. Donec quis massa mattis, ornare augue et, iaculis dui. Vivamus faucibus luctus malesuada. Phasellus non suscipit quam, ac consequat nunc. Vivamus ante lacus, malesuada et mauris a, faucibus semper arcu. Quisque ut erat est. Maecenas gravida, lorem in iaculis sodales, dui erat aliquet orci, id porttitor felis massa vitae lectus. Proin gravida sed urna in sagittis. In fringilla lectus id ipsum euismod volutpat. Nullam ac ante luctus, porttitor metus in, sodales neque.
+      </p>
+    </div>
+  </div>
+    </div>
+  </body>
+</html>
